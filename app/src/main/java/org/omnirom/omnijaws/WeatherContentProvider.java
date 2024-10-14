451e75744c884fe44d5b439338883eac76aadb5f
minaripenguin <minaripenguin@users.noreply.github.com>
2024-10-14 14:38:43 +0800

    Fix compilation
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- app/src/main/java/org/omnirom/omnijaws/WeatherContentProvider.java
+++ app/src/main/java/org/omnirom/omnijaws/WeatherContentProvider.java
@@ -15,7 +15,7 @@
  */
 package org.omnirom.omnijaws;
 
-import com.android.internal.util.crdroid.OmniJawsClient;
+import com.android.internal.util.android.OmniJawsClient;
 
 import org.omnirom.omnijaws.WeatherInfo.DayForecast;
 
